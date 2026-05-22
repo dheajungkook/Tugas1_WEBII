b5d081dfe2ef21b872764209df14cb1735fe19e1
dheajungkook <amitadhea425@gmail.com>
2026-05-22 18:23:45 +0700

    commit 2

--- resources/views/components/navadmin.blade.php
+++ resources/views/components/navadmin.blade.php
@@ -4,14 +4,17 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
-
+        
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/admin">dashboard</a>
+                    <a class="nav-link" href="/admin">Dashboard</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/admin/users">users</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/admin/product">Product</a>
                 </li>
             </ul>
         </div>
